332e0426023a2a78687d68a5f7dc8947c09b89a9
Maxim Bondarenco <Maxim.Bondarenco@endava.com>
2022-10-20 10:53:50 +0300

    basic input error handling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-advanced';
+  constructor(private fb: FormBuilder) {}
+
+  profileForm = this.fb.group({
+    username: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.minLength(8)],
+  });
+
+  onSubmit() {
+    console.log(this.profileForm.value);
+  }
 }
